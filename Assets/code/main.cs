dcaa4cedb69b09a9d3ad85e9be91abd5fbc2dd28
Andrew <duerig.andrew@gmail.com>
2020-09-11 21:29:59 -0400

    stuff

---
+++ Assets/code/main.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main : MonoBehaviour
+{
+    public GameObject main_char;
+
+    void Start()
+    {
+        
+    }
+
+    void Update () 
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) 
+        {
+            Instantiate(main_char);
+        }
+    }
+}
