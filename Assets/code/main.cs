8225331c5b13d3b2189c7be48eb840035cce06e6
Andrew <duerig.andrew@gmail.com>
2020-09-11 22:18:35 -0400

    mystuff

--- Assets/code/main.cs
+++ Assets/code/main.cs
@@ -17,5 +17,39 @@ public class main : MonoBehaviour
         {
             Instantiate(main_char);
         }
+
+
+    }
+}
+
+enum InputType 
+{
+  Left,
+  Right,
+  Jump
+}
+
+
+public class CharacterLife
+{
+    public ArrayList<Tuple<double, InputType>> inputs;
+    private int currentPositionInArray;
+    private GameObject unityObject;
+
+    public CharacterLife(GameObject unityObject)
+    {
+        inputs = new ArrayList<int>();
+        currentPositionInArray = 0;
+        unityObject = unityObject;
+    }
+
+    public TrackInput(double timeDelta, InputType input)
+    {
+        inputs.Add(new Tuple<double, InputType>(timeDelta, input));
+    }
+
+    public UpdateFromHistory(double timeDelta)
+    {
+        //
     }
 }
