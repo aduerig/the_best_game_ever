e5c918aeef58964cf7bd534386789263e451985e
Andrew <duerig.andrew@gmail.com>
2020-09-12 13:47:14 -0400

    lol

---
+++ Assets/code/LuigiController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LuigiController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionStay(Collision collision)
+    {
+        Debug.Log(collision.collider.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
