66091468d8ac0af034dee7a248f422fa506e8ef3
Mary Abbott <mabbott9@gatech.edu>
2020-09-12 23:10:31 -0400

    Button Door script

---
+++ Assets/code/ButtonDoorController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonDoorController : MonoBehaviour
+{
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        var doorObj = transform.parent.GetChild(transform.GetSiblingIndex() + 1);
+        var animators = gameObject.GetComponentsInParent<Animator>();
+        foreach (var animator in animators)
+        {
+            animator.SetBool("isPushed", true);
+        }
+        doorObj.GetComponent<BoxCollider2D>().enabled = false; // TODO: does this open all doors?
+        
+    }
+}
