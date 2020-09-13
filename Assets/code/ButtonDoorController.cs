c47eb4c22200e89fc7ba5da3767c81c2f89da512
Mary Abbott <mabbott9@gatech.edu>
2020-09-13 00:27:18 -0400

    Full button and door prefab

--- Assets/code/ButtonDoorController.cs
+++ Assets/code/ButtonDoorController.cs
@@ -21,12 +21,55 @@ public class ButtonDoorController : MonoBehaviour
     void OnTriggerStay2D(Collider2D other)
     {
         var doorObj = transform.parent.GetChild(transform.GetSiblingIndex() + 1);
-        var animators = gameObject.GetComponentsInParent<Animator>();
-        foreach (var animator in animators)
+        var animator = gameObject.GetComponent<Animator>();
+        if (animator != null)
+        {
+            animator.SetBool("isPushed", true);
+        }
+        var spriteSize = gameObject.GetComponent<SpriteRenderer>().sprite.bounds.size;
+        var boxes = gameObject.GetComponents<BoxCollider2D>();
+        foreach (var box in boxes)
+        {
+            if (!box.isTrigger)
+            {
+                box.size = spriteSize;
+                box.offset = new Vector2(0, 0.5f - (1 - spriteSize.y) / 2);
+            }
+        }
+
+        animator = doorObj.GetComponent<Animator>();
+        if (animator != null)
         {
             animator.SetBool("isPushed", true);
         }
         doorObj.GetComponent<BoxCollider2D>().enabled = false; // TODO: does this open all doors?
         
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        var doorObj = transform.parent.GetChild(transform.GetSiblingIndex() + 1);
+        var animator = gameObject.GetComponent<Animator>();
+        if (animator != null)
+        {
+            animator.SetBool("isPushed", false);
+        }
+        var spriteSize = gameObject.GetComponent<SpriteRenderer>().sprite.bounds.size;
+        var boxes = gameObject.GetComponents<BoxCollider2D>();
+        foreach (var box in boxes)
+        {
+            if (!box.isTrigger)
+            {
+                box.size = spriteSize;
+                box.offset = new Vector2(0, 0.5f - (1 - spriteSize.y) / 2);
+            }
+        }
+
+        animator = doorObj.GetComponent<Animator>();
+        if (animator != null)
+        {
+            animator.SetBool("isPushed", false);
+        }
+        doorObj.GetComponent<BoxCollider2D>().enabled = true; // TODO: does this open all doors?
     }
 }
